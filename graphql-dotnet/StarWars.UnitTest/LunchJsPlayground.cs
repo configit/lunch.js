3d7a192d477fb52460a5bc1bdf95b28e42e89ccf
Morten Maate <mhm@configit.com>
2016-02-12 15:26:44 +0100

    Powerpoint and LunchJS Playground for GraphQL

---
+++ graphql-dotnet/StarWars.UnitTest/LunchJsPlayground.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace StarWars.UnitTest {
+  [TestFixture]
+  public class LunchJsPlayground {
+    [SetUp]
+    public void SetUp() {
+      _api = new Api();
+    }
+
+    private Api _api;
+
+    private void Execute( string query, string inputs = null ) {
+      string result = _api.Execute( query, inputs );
+      Console.WriteLine( JsonHelper.FormatJson( result ) );
+    }
+
+    [Test]
+    public void Query() {
+      const string Q = @"
+query q {
+  humans( count: 1 ) {
+    name
+  }
+}
+
+
+";
+
+      Execute( Q );
+    }
+  }
+}
